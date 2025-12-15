adf083d9c86f101f4a0d2f8fdd978e0d9b9fbe3c
DenysLobur <denismachtalles@gmail.com>
2025-12-15 11:08:53 +0200

    Checking OneToOne relation

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\UserProfileRepository;
 use Doctrine\DBAL\Query\Limit;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,14 +18,26 @@ class HelloController extends AbstractController
         ['message' => 'Bye!', 'created' => '2024/05/12']
     ];
 
-    #[Route('/{limit}', name: 'app_index', requirements: ['limit' => '\\d+'], defaults: ['limit' => 3])]
-    public function index(int $limit): Response
+    #[Route('/', name: 'app_index')]
+    public function index(UserProfileRepository $profiles): Response
     {
+        // $user = new User();
+        // $user->setEmail('email@email.com');
+        // $user->setPassword('12345678');
+
+
+        // $profile = new UserProfile();
+        // $profile->setUser($user);
+        // $profiles->add($profile, true);
+
+        // $profile = $profiles->find(1);
+        // $profiles->remove($profile, true);
+
         return $this -> render(
             'hello/index.html.twig',
             [
                 'messages' => $this->messages,
-                'limit' => $limit
+                'limit' => 3
             ]
         );
 
