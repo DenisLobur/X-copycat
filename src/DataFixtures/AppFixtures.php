bf00d66dce00adcba1fcb82acdd4b156aaf4104a
DenysLobur <denismachtalles@gmail.com>
2025-12-15 17:27:09 +0200

    Add User Command

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,45 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\MicroPost;
 use DateTime;
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
+use App\Entity\MicroPost;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    )
+    {
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $user1 = new User();
+        $user1->setEmail('test@test.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user1,
+                '12345678'
+            )
+        );
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('test2@test2.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user2,
+                '12345678'
+            )
+        );
+        $manager->persist($user2);
+
+
         // $product = new Product();
         // $manager->persist($product);
         $microPost1 = new MicroPost();
