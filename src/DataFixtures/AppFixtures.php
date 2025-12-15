28642addfbc1944d0be77a15eac4cfc0dac65eb0
DenysLobur <denismachtalles@gmail.com>
2025-12-15 22:50:05 +0200

    Relate Posts and Comments to User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -47,18 +47,21 @@ class AppFixtures extends Fixture
         $microPost1->setTitle('Welcome welcome easy goer');
         $microPost1->setText('This is an ice cold');
         $microPost1->setCreated(new DateTime());
+        $microPost1->setAuthor($user1);
         $manager->persist($microPost1);
 
         $microPost2 = new MicroPost();
         $microPost2->setTitle('Welcome welcome pumpy dump');
         $microPost2->setText('This is a hot one');
         $microPost2->setCreated(new DateTime());
+        $microPost2->setAuthor($user1);
         $manager->persist($microPost2);
 
         $microPost3 = new MicroPost();
         $microPost3->setTitle('Welcome just saying');
         $microPost3->setText('This is a third row');
         $microPost3->setCreated(new DateTime());
+        $microPost3->setAuthor($user1);
         $manager->persist($microPost3);
 
         $manager->flush();
